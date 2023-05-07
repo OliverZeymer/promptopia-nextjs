b8f26dc23a9ad8c336829d98e595e76740e34107
OtakuBoy420 <U28282@rts-365.dk>
2023-05-07 22:35:41 +0200

    feed, next auth, google login and copy to clipboard added

---
+++ components/Loader.jsx
@@ -0,0 +1,11 @@
+export default function Loader({ size }) {
+  return (
+    <div
+      className={
+        size === "lg"
+          ? "m-auto h-20 w-20 animate-spin rounded-full border-4 border-solid border-white border-t-transparent"
+          : "m-auto h-5 w-5 animate-spin rounded-full border-2 border-solid border-white border-t-transparent"
+      }
+    />
+  );
+}
